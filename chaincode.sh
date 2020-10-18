cf04153f88be185bac7955b64cfbcdb2450b1146
Your Name <mpc@Blackpool>
2020-10-18 16:23:46 +0800

    fix

--- chaincode.sh
+++ chaincode.sh
@@ -1,3 +1,6 @@
-//these steps run after generating enclave.signed.so
+#these steps run after generating enclave.signed.so
 docker cp /home/mpc/go/src/github.com/sgx-ra/Enclave/Enclave.signed.so 83517eae918a:/usr/local/bin
 docker cp /home/mpc/go/src/github.com/sgx-ra/demo_sgx/sealeddata.bin 83517eae918a:/usr/local/bin
+#to regenerate libapp.a
+cp /home/mpc/go/src/github.com/sgx-ra/Enclave/Enclave.signed.so /home/mpc/go/src/github.com/sgx-ra/demo_sgx/sgx-compute/app
+cp /home/mpc/go/src/github.com/sgx-ra/demo_sgx/sealeddata.bin /home/mpc/go/src/github.com/sgx-ra/demo_sgx/sgx-compute/app
